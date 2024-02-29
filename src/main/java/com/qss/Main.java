02bf29bc93acbc31b005aef8e4be3adc132d1dfc
心的征程 <1623995997@qq.com>
2024-02-29 10:07:31 +0800

    初始化项目

--- src/main/java/com/qss/Main.java
+++ src/main/java/com/qss/Main.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         SpringApplication.run(Main.class, args);
     }
 }

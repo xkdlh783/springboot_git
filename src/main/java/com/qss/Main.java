57679c66c8085106f6f8ebab277f8b23fe77f4db
心的征程 <1623995997@qq.com>
2024-02-29 10:25:53 +0800

    master提交

--- src/main/java/com/qss/Main.java
+++ src/main/java/com/qss/Main.java
@@ -8,5 +8,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         SpringApplication.run(Main.class, args);
+        System.out.println("master commit");
     }
 }

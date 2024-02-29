c3dc55258ae8499bae4263b36d359998eef30a2a
心的征程 <1623995997@qq.com>
2024-02-29 10:07:52 +0800

    Revert "初始化项目"
    This reverts commit 02bf29bc93acbc31b005aef8e4be3adc132d1dfc.

--- src/main/java/com/qss/Main.java
+++ src/main/java/com/qss/Main.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
         SpringApplication.run(Main.class, args);
     }
 }

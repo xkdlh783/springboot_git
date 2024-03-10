26bd14a8dec6dc347875c21a7728606a7e173d1b
我的名字 <1623@qq.com>
2024-03-10 21:19:28 +0800

    4

--- src/main/java/com/qss/Main.java
+++ src/main/java/com/qss/Main.java
@@ -14,5 +14,6 @@ public class Main {
         System.out.println("dev 修复404");
         System.out.println("dev 修复405");
         System.out.println("dev 待提交内容");
+        System.out.println("GitHub");
     }
 }
